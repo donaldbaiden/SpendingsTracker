c04324cdd2e95e0d665868ec2e259ee8786b83b2
donaldbaiden <fakefriks.gg@gmail.com>
2024-05-23 10:22:48 +0300

    Initial Commit

---
+++ SpendingsTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendingsTracker
+//
+//  Created by Михаил Корузн on 23.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendingsTracker/SpendingsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingsTrackerApp.swift
+//  SpendingsTracker
+//
+//  Created by Михаил Корузн on 23.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SpendingsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
